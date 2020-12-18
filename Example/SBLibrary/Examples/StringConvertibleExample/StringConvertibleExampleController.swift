4ad3fa12ca6cf90246a76b31ad5b6b659074729b
ShenYj <shenyanjie123@foxmail.com>
2020-12-18 23:27:12 +0800

    demo for StringConvertible

--- Example/SBLibrary/Examples/StringConvertibleExample/StringConvertibleExampleController.swift
+++ Example/SBLibrary/Examples/StringConvertibleExample/StringConvertibleExampleController.swift
@@ -7,9 +7,29 @@
 //
 
 import UIKit
+import SBLibrary
 
 internal class StringConvertibleExampleController: UIViewController {
     
+    @IBOutlet weak var withWhitespaaceLB: UILabel!
+    @IBOutlet weak var withNewlineLB: UILabel!
+    @IBOutlet weak var withWhitespaceAndNewlineLB: UILabel!
+    
+    @IBOutlet weak var withoutWhitespaceLB: UILabel!
+    @IBOutlet weak var withoutNewlineLB: UILabel!
+    @IBOutlet weak var withoutWhitespaceAndNewlineLB: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        withWhitespaaceLB.text = "包含空格:注意 这里 有 空 格"
+        withNewlineLB.text = "包含换行符: \n测试字符串"
+        withWhitespaceAndNewlineLB.text = "包含换行符和空格: \n 测  试字符串"
+        
+        withoutWhitespaceLB.text = withWhitespaaceLB.text!.withoutSpace
+        withoutNewlineLB.text = withNewlineLB.text!.withoutNewLine
+        withoutWhitespaceAndNewlineLB.text = withWhitespaceAndNewlineLB.text!.withoutSpaceAndNewLine
+    }
 }
 
 extension StringConvertibleExampleController: ExampleController { }
